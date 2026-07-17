9acccf049159a21cadd9f12aad5e545c0e666cd3
Chorty <matt.joslin@me.com>
2026-07-16 21:19:39 -0400

    fix: use deployed video monitor path

--- backend/VacuumstreamerExtensions.js
+++ backend/VacuumstreamerExtensions.js
@@ -2,19 +2,19 @@ const capabilities = require("./dreame-capabilities");
 
 module.exports = function registerVacuumstreamerCapabilities(robot) {
     robot.registerCapability(new capabilities.DreameVideoStreamCapability({
-        robot,
+        robot: robot,
         streamConfig: {
             vacuumstreamerPath: "/data/vacuumstreamer/vacuumstreamer.so",
             go2rtcPath: "/data/vacuumstreamer/go2rtc",
             go2rtcConfigPath: "/data/vacuumstreamer/go2rtc.yaml",
-            videoMonitorPath: "/usr/bin/video_monitor",
+            videoMonitorPath: "/data/vacuumstreamer/video_monitor",
             udpPort: 6969,
             go2rtcApiPort: 1984,
         },
     }));
 
     robot.registerCapability(new capabilities.DreameTextToSpeechCapability({
-        robot,
+        robot: robot,
         ttsConfig: {
             tempDir: "/tmp",
             playerCommand: "aplay",
@@ -24,7 +24,7 @@ module.exports = function registerVacuumstreamerCapabilities(robot) {
     }));
 
     robot.registerCapability(new capabilities.DreameMapManagementCapability({
-        robot,
+        robot: robot,
         mapConfig: {
             storagePath: "/data/maploader",
             mapDirs: ["/data/ri", "/data/map", "/data/DivideMap"],
